057b88b6e9997c318c59c7e3f5652b5e37140226
rofl0r <retnyg@gmx.net>
2011-07-21 04:33:51 +0200

    those hacks seem to be causing problems. builds like that now.

--- KEEP/openssh-hacks.h
+++ KEEP/openssh-hacks.h
@@ -3,8 +3,8 @@
 #include <sys/time.h>
 #include <sys/param.h>
 
-#define ut_addr     ut_addr_v6[0]
+//#define ut_addr     ut_addr_v6[0]
 
 /* else ssh-keygen doesn't build. */
-static inline int pkcs11_init(int i) { return 0; }
-static inline void pkcs11_terminate() { }
+//static inline int pkcs11_init(int i) { return 0; }
+//static inline void pkcs11_terminate() { }
